ba3552f6c581e5271ab311c7375841a12c5700ff
Dylan Hoffman <jkioicefire@gmail.com>
2022-04-18 06:19:40 -0500

    first commit

---
+++ utils.js
@@ -0,0 +1,98 @@
+import 'dotenv/config';
+import fetch from 'node-fetch';
+import { verifyKey } from 'discord-interactions';
+
+//need to import google apis here
+
+export function VerifyDiscordRequest(clientKey) {
+  return function (req, res, buf, encoding) {
+    const signature = req.get('X-Signature-Ed25519');
+    const timestamp = req.get('X-Signature-Timestamp');
+
+    const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
+    if (!isValidRequest) {
+      res.status(401).send('Bad request signature');
+      throw new Error('Bad request signature');
+    }
+  };
+}
+
+export async function DiscordRequest(endpoint, options) {
+  // append endpoint to root API URL
+  const url = 'https://discord.com/api/v9/' + endpoint;
+  console.log(endpoint)
+  // Stringify payloads
+  if (options.body) options.body = JSON.stringify(options.body);
+  // Use node-fetch to make requests
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    ...options
+  });
+  // throw API errors
+  if (!res.ok) {
+    const data = await res.json();
+    console.log(res.status);
+    throw new Error(JSON.stringify(data));
+  }
+  // return original response
+  return res;
+}
+
+// Simple method that returns a random emoji from list
+export function getRandomEmoji() {
+  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
+  return emojiList[Math.floor(Math.random() * emojiList.length)];
+}
+
+export function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+
+export async function getVideo(search){
+
+  const url = "https://www.googleapis.com/youtube/v3/{search}&key=" + process.env.YOUTUBE_KEY;
+  await fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  console.log(data);
+
+  return data[0];
+
+}
+
+
+//youtube functions
+export function authenticate() {
+  return gapi.auth2.getAuthInstance()
+      .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+      .then(function() { console.log("Sign-in successful"); },
+            function(err) { console.error("Error signing in", err); });
+}
+export function loadClient() {
+  gapi.client.setApiKey(process.env.YOUTUBE_KEY);
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+      .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+}
+// Make sure the client is loaded and sign-in is complete before calling this method.
+export function execute() {
+  return gapi.client.youtube.search.list({
+    "part": [
+      "utxo bitcoin"
+    ]
+  })
+      .then(function(response) {
+              // Handle the results here (response.result has the parsed body).
+              console.log("Response", response);
+            },
+            function(err) { console.error("Execute error", err); });
+}
+gapi.load("client:auth2", function() {
+  gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+});
---
+++ video_player.js
@@ -0,0 +1,14 @@
+
+
+// export async function getVideo(search){
+
+//     const url = "https://www.googleapis.com/youtube/v3/{search}";
+//     await fetch(url)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+//     console.log(data);
+
+//     return data[0];
+
+// }
