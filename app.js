72c89848ae771399f2a0e888de65c298a07de5b9
Dylan Hoffman <jkioicefire.gmail.com>
2022-05-05 06:47:46 -0500

    added functionality to get channel from discord client

--- app.js
+++ app.js
@@ -61,17 +61,17 @@ app.post('/interactions', async function (req, res) {
     const opts = data.options[0];
     const value = opts.value;
 
-    console.log("Request_Body:")
-    console.log(req.body);
+    // console.log("Request_Body:")
+    // console.log(req.body);
 
-    console.log("Member:")
-    console.log(member);
+    // console.log("Member:")
+    // console.log(member);
 
     const user_id = member.user.id;
-    console.log("User ID:")
-    console.log(user_id);
-    console.log("Guild ID:")
-    console.log(guild_id);
+    // console.log("User ID:")
+    // console.log(user_id);
+    // console.log("Guild ID:")
+    // console.log(guild_id);
 
     if (name === 'play') {
       console.log("Search:")
@@ -79,12 +79,12 @@ app.post('/interactions', async function (req, res) {
       
       // const video = await getVideo(value);
       let video = await getVideo(value);
-      console.log("Youtube Video:")
-       console.log(video)
+      // console.log("Youtube Video:")
+      //  console.log(video)
 
        let youtube_id = video.items[0].id.videoId;
-       console.log("Youtube ID:")
-      console.log(youtube_id);
+      //  console.log("Youtube ID:")
+      // console.log(youtube_id);
 
       if(video != null){
         // video = `<iframe src="https://www.youtube.com/watch?v=${youtube_id}>`
@@ -104,17 +104,29 @@ app.post('/interactions', async function (req, res) {
         throw new Error("Channel name not found. exiting")
       }
 
-      const guild = await GetGuild(guild_id);
-      console.log("Guild stringify:")
-      console.log(guild);
-      const guildObject = JSON.parse(`"${guild}"`);
-      console.log("Guild:")
-      console.log(guildObject);
+      // const guild = await GetGuild(guild_id);
+      // console.log("Guild stringify:")
+      // console.log(guild);
+      // const guildObject = JSON.parse(`"${guild}"`);
+      // console.log("Guild:")
+      // console.log(guildObject);
 
-      const guildManager = client.guilds;
-      const newguild = await guildManager.fetch(guild_id)
+      //get a guild object for discord.js
+      // const guildManager = client.guilds;
+
+      //get the guild object which is needed for the queue
+      const newguild = await client.guilds.fetch(guild_id)
+
+      //get the channel object which is needed for the queue
+      const newChannel = await client.channels.fetch(channelID);
+                          // .then(channel => console.log(channel.name))
+                          // .catch(console.error);
+
+      console.log("channels");
+      console.log(newChannel);
       console.log("new guild");
       console.log(newguild);
+
       //move member functionality if needed
       // const moveMemberURL = `guilds/${guild_id}/members/${user_id}`;
       // const moveMemberRequest = await DiscordRequest(moveMemberURL, {method: 'PATCH', body: {channel_id: channelID}});
@@ -125,7 +137,7 @@ app.post('/interactions', async function (req, res) {
       const query = `https://www.youtube.com/watch?v=${youtube_id}>`;
       const queue = player.createQueue(newguild, {
         metadata: {
-          channel: channel
+          channel: newChannel
         }
       });
 
