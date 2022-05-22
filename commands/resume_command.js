ba1a2003de6b99e3d527ab8de3e96f7a8b48db9a
Dylan Hoffman <jkioicefire.gmail.com>
2022-05-22 14:36:43 -0500

    added pause and resume commands

---
+++ commands/resume_command.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("resume")
+    .setDescription('resumes the currently playing song'),
+    async execute(interaction, player){
+        console.log(interaction);
+
+        if (!interaction.member.voice.channelId) {
+            return await interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
+        }
+        if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) {
+            return await interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
+        }
+
+        const queue = player.getQueue(interaction.guild);
+
+        if(queue === null || queue === undefined){
+            return await  interaction.reply({content: "Queue has not been created yet, use the /play command to get the party started!"});
+        }
+
+        if (!queue.nowPlaying()) {
+            return await interaction.reply({ content: "Queue is not currently active", ephemeral: true });
+        }
+        // verify vc connection
+        try {
+            if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+        } catch {
+            queue.destroy();
+            return await interaction.reply({ content: "Could not join your voice channel!", ephemeral: true });
+        }
+
+        // queue.clear();
+        queue.setPaused(false);
+        return await interaction.reply({ content: "Bot is resuming"});
+    }
+}
